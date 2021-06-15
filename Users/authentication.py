8c3cd78d668b88b13de65c509d9dc3868b05b67f
Maria Nidchenko <marianidchenko@gmail.com>
2021-06-15 13:02:41 +0300

    Initial commit

---
+++ Users/authentication.py
@@ -0,0 +1,45 @@
+import json
+from tkinter import messagebox
+
+
+def log_in(username, password):
+    with open('Users/users.txt', 'r') as file:
+        usernames = json.loads(file.read())
+        if username in usernames:
+            with open('Users/credentials.txt', 'r') as password_file:
+                credentials = json.loads(password_file.read())
+                if password == credentials[username]:
+                    pass # take to application
+                else:
+                    messagebox.showinfo(' ', 'Password is incorrect!')
+                    return
+        else:
+            messagebox.showinfo(' ', 'Username is not registered. Please try again or create an account.')
+
+
+def register_user(username, password, password_confirmation):
+    try:
+        with open('Users/credentials.txt', 'r') as file:
+            credentials = json.loads(file.read())
+    except:
+        credentials = {}
+
+    try:
+        with open('Users/users.txt', 'r') as file:
+            users = json.loads(file.read())
+    except:
+        users = []
+
+    if password_confirmation == password:
+        if username not in credentials.keys():
+            credentials[username] = password
+            users.append(username)
+            with open('Users/credentials.txt', 'w') as file:
+                json.dump(credentials, file)
+            with open('Users/users.txt', 'w') as file:
+                json.dump(users, file)
+            messagebox.showinfo(' ', 'Registered successfully!')
+        else:
+            messagebox.showinfo(' ', 'Already registered. Please log in.')
+    else:
+        messagebox.showinfo(' ', 'Passwords do not match.')
---
+++ canvas.py
@@ -0,0 +1,62 @@
+from tkinter import *
+from Users.authentication import *
+
+
+def clear_view():
+    for slave in window.grid_slaves():
+        slave.destroy()
+
+
+def login_or_register():
+    clear_view()
+    login = Button(window, text='Login', command=lambda: render_login_view())
+    login.grid(column=0, row=0, padx=5, pady=5)
+    register = Button(window, text='Register', command=lambda: render_register_view())
+    register.grid(column=1, row=0, padx=5, pady=5)
+
+
+def render_login_view():
+    clear_view()
+    # Username Entry
+    Label(window, text="Username:").grid(column=0, row=0, padx=5, pady=5)
+    username = Entry(window, width=30)
+    username.grid(column=1, row=0, padx=5, pady=5)
+    # Password Entry
+    Label(window, text='Password:').grid(column=0, row=1, padx=5, pady=5)
+    password = Entry(window, width=30, show='*')
+    password.grid(column=1, row=1, padx=5, pady=5)
+    # Login Button
+    login_button = Button(window, text='Login', command=lambda: log_in(username.get(), password.get()))
+    login_button.grid(column=1, row=2, padx=5, pady=5)
+
+
+def render_register_view():
+    clear_view()
+    # Username Entry
+    Label(window, text="Username:").grid(column=0, row=0, padx=5, pady=5)
+    username = Entry(window, width=30)
+    username.grid(column=1, row=0, padx=5, pady=5)
+    # Password Entry
+    Label(window, text='Password:').grid(column=0, row=1, padx=5, pady=5)
+    password = Entry(window, width=30, show='*')
+    password.grid(column=1, row=1, padx=5, pady=5)
+    # Password confirmation
+    Label(window, text='Confirm Password:').grid(column=0, row=2, padx=5, pady=5)
+    password_confirmation = Entry(window, width=30, show='*')
+    password_confirmation.grid(column=1, row=2, padx=5, pady=5)
+    # Register Button
+    register_button = Button(window, text='Register',
+                             command=lambda: register_user(username.get(), password.get(), password_confirmation.get()))
+    register_button.grid(column=1, row=3, padx=5, pady=5)
+    # Login Button
+    login_button = Button(window, text='Login', command=lambda: render_login_view())
+    login_button.grid(column=0, row=3, padx=5, pady=5)
+
+
+if __name__ == '__main__':
+    window = Tk()
+    window.title('GUI Shop')
+    window.geometry("800x600")  # window size
+    login_or_register()
+    window.mainloop()
+
